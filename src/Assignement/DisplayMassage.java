b5421551f814a98488bccda17de852556e1cf45f
Supriya Bhurle <supriyabhurle28@gmail.com>
2022-11-12 16:05:22 +0530

    First commit

---
+++ src/Assignement/DisplayMassage.java
@@ -0,0 +1,7 @@
+package Assignement;
+
+public class DisplayMassage {
+    public static void main(String[] args) {
+        System.out.print("Hlo How are you?");
+    }
+}
---
+++ src/Assignement/DisplayNameUsingCommandline.java
@@ -0,0 +1,17 @@
+package Assignement;
+
+public class DisplayNameUsingCommandline {
+    public static void main(String[] args){
+
+        System.out.println(args[0] + " ");
+        System.out.println(args[1] + " ");
+        System.out.println(args[2] + " ");
+        System.out.println(args[3] + " ");
+
+        }
+
+        
+
+
+    }
+
